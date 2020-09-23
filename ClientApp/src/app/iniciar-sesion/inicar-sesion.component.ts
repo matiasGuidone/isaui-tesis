f8c2c6ac3da512ec5f8bcd0be3b429e911f7c1c9
matiasGuidone <matias.guidone@gmail.com>
2020-09-22 23:29:39 -0300

    agregado de guard en el module

--- ClientApp/src/app/iniciar-sesion/inicar-sesion.component.ts
+++ ClientApp/src/app/iniciar-sesion/inicar-sesion.component.ts
@@ -20,6 +20,7 @@ nClave: string;
     const user= {nombre: this.nCuenta, codigo: this.nClave};
     this.autS.login(user).subscribe(res =>{
       console.log(res)
+      this.router.navigate(['/']);
     },
     er=>console.log(er)
       );
